d8077a8802005815b7906810d110dcc57f106d5f
Julien <julien.prodhomme@hotmail.fr>
2021-02-05 20:56:25 +0100

    first commit

---
+++ src/main/java/com/ittraining/controllers/FormateurController.java
@@ -0,0 +1,39 @@
+package com.ittraining.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.ittraining.entities.Formateur;
+import com.ittraining.services.FormateurService;
+
+public class FormateurController {
+	
+	@Autowired
+	private FormateurService service;
+	
+	@PostMapping("")
+	public Formateur save(@RequestBody Formateur entity) {
+		return this.service.save(entity);
+	}
+
+	@GetMapping("nom/{nom}")
+	public List<Formateur> findByNom(@PathVariable String nom) {
+		return service.findByNom(nom);
+	}
+
+	@GetMapping("note/{note}")
+	public List<Formateur> findByNote(@PathVariable int note) {
+		return service.findByNote(note);
+	}
+
+	@GetMapping("{id}")
+	public Formateur findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+}
---
+++ src/main/java/com/ittraining/entities/Formateur.java
@@ -0,0 +1,62 @@
+package com.ittraining.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "formateurs")
+public class Formateur {
+	
+	
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private Long id;
+		
+		@Column(name = "nom")
+		private String titre;
+
+		@Column(name = "note")
+		private int note;
+		
+		/*@ManyToMany
+		@JoinTable(
+				name = "formateur_has_eval_session",
+				joinColumns = @JoinColumn(name = "formateur_id"),
+				inverseJoinColumns = @JoinColumn(name = "eval_session_id"))*/
+		//private List<eval_session> eval = new ArrayList<>();
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitre() {
+			return titre;
+		}
+
+		public void setTitre(String titre) {
+			this.titre = titre;
+		}
+
+		public int getNote() {
+			return note;
+		}
+
+		public void setNote(int note) {
+			this.note = note;
+		}
+		
+		
+		
+		
+
+	}
